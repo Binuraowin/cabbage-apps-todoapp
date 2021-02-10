eea5be3182979203fba0755fb1390ec270a0573a
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2021-02-11 02:13:12 +0530

    Update AddTodo.tsx

--- cabbage-todoapp/src/Components/addTodo/AddTodo.tsx
+++ cabbage-todoapp/src/Components/addTodo/AddTodo.tsx
@@ -20,10 +20,11 @@ handleSubmitTodo,
       type="text"
       name="task"
       value={task}
+      className="flex-1 rounded shadow p-2 text-grey-dark mr-2"
       onChange={handleChange}
     />
     <Button onClick={handleSubmitTodo} type={"dashed"} aria-label="Add todo">
-        <PlusOutlined  />
+        <PlusOutlined className="shadow-sm border-none h-10 w-7 flex h-7 w-7 flex justify-center items-center" />
     </Button>
   </form>
 )
