76c2f8505c6c6f279d3990ff901fdf07f157e9aa
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2021-02-11 01:34:52 +0530

    basic structure

---
+++ cabbage-todoapp/src/Components/Todos.tsx
@@ -0,0 +1,12 @@
+import { Row } from "./row/Row"
+import { data } from "../todos"
+
+export const Todos = () =>{
+    return (
+        <section>
+            {data.map((todo) => (
+                <Row key={todo.id}  todo={todo}/>
+            ))}
+        </section>
+    )
+}
